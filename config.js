10aee9ae1d1c6eddfd64ce2d3afe22ae06700d28
Md. Sohan Talukder Akash <sohan15-1933@diu.edu.bd>
2023-06-06 23:31:32 +0600

    add react project

--- config.js
+++
@@ -1,5 +0,0 @@
-export const BASE_URL = process.env.NEXT_PUBLIC_BASE_API_URL;
-export const production_mode = process.env.NEXT_PUBLIC_NODE_ENV;
-export const BASE_API_URL = `${BASE_URL}/api/v1`;
-export const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID;
-export const GOOGLE_CLIENT_SECRET = process.env.NEXT_PUBLIC_CLIENT_SECRET;
