ce04dbc4d2f8e2eaf41d100302ad83be824e5688
Md. Sohan Talukder Akash <sohan15-1933@diu.edu.bd>
2023-06-09 21:56:51 +0600

    add seo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import NavBar from "./components/navbar/Navbar";
 import { Toaster } from "react-hot-toast";
 import ScrollTop from "./components/scrollTop/ScrollTop";
 import Footer from "./components/footer/Footer.jsx";
+import { HelmetProvider } from "react-helmet-async";
+import SEO from "./utils/seo/SEO";
 
 function App() {
     return (
-        <>
+        <HelmetProvider>
+            <SEO
+                title='Tourist Guide'
+                description='Discover and explore your dream destinations with our tourist guide website. Immerse yourself in local culture, uncover hidden gems, and visit iconic landmarks. Enhance your travel experience by hiring experienced guides through our platform. Create unforgettable memories with us as your trusted companion on your next adventure.'
+                name='Tourist Guide'
+                type='travel, tour, tourist, guide'
+            />
             <NavBar />
             {<Outlet />}
             <Toaster
@@ -27,7 +35,7 @@ function App() {
             />
             <ScrollTop />
             <Footer />
-        </>
+        </HelmetProvider>
     );
 }
 
--- src/authentication/login/Login.jsx
+++ src/authentication/login/Login.jsx
@@ -11,6 +11,7 @@ import Image from "../../components/image/Image";
 import CustomInput from "../../components/customInput/CustomInput";
 import { useLoginMutation } from "../../redux/slice/authentication/authApi";
 import { Link } from "react-router-dom";
+import SEO from "../../utils/seo/SEO";
 const initialValues = {
     email: "",
     password: "",
@@ -52,6 +53,12 @@ const Login = () => {
     }, [user]);
     return (
         <div>
+            <SEO
+                title='Login'
+                description='Discover and explore your dream destinations with our tourist guide website. Immerse yourself in local culture, uncover hidden gems, and visit iconic landmarks. Enhance your travel experience by hiring experienced guides through our platform. Create unforgettable memories with us as your trusted companion on your next adventure.'
+                name='Tourist Guide'
+                type='travel, tour, tourist, guide'
+            />
             <div className='container mx-auto max-w-[1180px] px-4 lg:px-0 py-16'>
                 <div className='flex flex-col lg:flex-row items-center justify-center'>
                     <div className='w-full lg:w-[537px] hidden lg:block'>
